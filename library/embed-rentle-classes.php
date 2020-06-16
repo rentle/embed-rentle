ec9a3a72cb2fc5d0cba4a97d6959a05f6edc4350
Timo Leppänen <timo@luuptek.fi>
2020-06-16 12:17:58 +0300

    support for lang attribute, generates automaticaly / small fixing

--- library/embed-rentle-classes.php
+++ library/embed-rentle-classes.php
@@ -2,15 +2,15 @@
 
 class Rentle_Content_Creator {
 	public static function create_rentle_content( $attributes ) {
-		$attributes = self::change_keys($attributes); //if this is a shortcode render, need to modify keys
+		$attributes = self::change_keys( $attributes ); //if this is a shortcode render, need to modify keys
 		?>
 		<div class="<?php echo self::get_classes( $attributes ) ?>">
 			<?php
-			if ( empty( $attributes['shopId'] ) || empty( $attributes['locationId'] ) ) {
+			if ( empty( $attributes['shopId'] ) ) {
 				self::errorrr();
 			} else {
 				?>
-				<iframe width="100%" height="100%" src="<?php echo self::create_iframe_src( $attributes ) ; ?>"></iframe>
+				<iframe width="100%" height="100%" src="<?php echo self::create_iframe_src( $attributes ); ?>"></iframe>
 				<?php
 			}
 			?>
@@ -51,12 +51,26 @@ class Rentle_Content_Creator {
 				$url .= "categories/${attributes['categoryId']}/";
 
 				if ( ! empty( $attributes['productId'] ) ) {
-					$url .= "products/${attributes['productId']}";
+					$url .= "products/${attributes['productId']}/";
 				}
 			}
 		}
 
+		// Adding lang code
+		$url .= self::setting_iframe_locale();
+
 		return $url;
+	}
+
+	private static function setting_iframe_locale() {
+		$string = '?lang=' . get_locale();
+
+		// Look for polylang installation
+		if ( function_exists( 'pll_current_language' ) ) {
+			$string = '?lang=' . pll_current_language();
+		}
+
+		return $string;
 	}
 
 	private static function errorrr() {
@@ -79,35 +93,35 @@ class Rentle_Content_Creator {
 	 *
 	 * @return mixed
 	 */
-	private static function change_keys($atts) {
-		if (isset($atts['shop'])) {
+	private static function change_keys( $atts ) {
+		if ( isset( $atts['shop'] ) ) {
 			$atts['shopId'] = $atts['shop'];
-			unset($atts['shop']);
+			unset( $atts['shop'] );
 		}
 
-		if (isset($atts['location'])) {
+		if ( isset( $atts['location'] ) ) {
 			$atts['locationId'] = $atts['location'];
-			unset($atts['location']);
+			unset( $atts['location'] );
 		}
 
-		if (isset($atts['category'])) {
+		if ( isset( $atts['category'] ) ) {
 			$atts['categoryId'] = $atts['category'];
-			unset($atts['category']);
+			unset( $atts['category'] );
 		}
 
-		if (isset($atts['product'])) {
+		if ( isset( $atts['product'] ) ) {
 			$atts['productId'] = $atts['product'];
-			unset($atts['product']);
+			unset( $atts['product'] );
 		}
 
-		if (isset($atts['padding'])) {
+		if ( isset( $atts['padding'] ) ) {
 			$atts['paddingSize'] = $atts['padding'];
-			unset($atts['padding']);
+			unset( $atts['padding'] );
 		}
 
-		if (isset($atts['class'])) {
+		if ( isset( $atts['class'] ) ) {
 			$atts['className'] = $atts['class'];
-			unset($atts['class']);
+			unset( $atts['class'] );
 		}
 
 		return $atts;
